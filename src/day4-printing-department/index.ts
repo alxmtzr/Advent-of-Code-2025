bb8cc9fe946240acdf5c7aba3bd0d8610ce6473c
alxmtzr <alxmtzr.dev@gmail.com>
2025-12-06 13:36:23 +0100

    prepare puzzle day4

--- src/day4-printing-department/index.ts
+++ src/day4-printing-department/index.ts
@@ -1,1 +1,16 @@
 import { readInput } from '../utils/InputReader';
+
+function getPaperGrid(): string[][] {
+    const lines = readInput.lines(4);
+    const grid: string[][] = lines.map((line: string) => line.split(''));
+
+    return grid;
+}
+
+function printPaperGrid(grid: string[][]) {
+    for (const row of grid) {
+        console.log(row.join(''));
+    }
+}
+
+printPaperGrid(getPaperGrid());
