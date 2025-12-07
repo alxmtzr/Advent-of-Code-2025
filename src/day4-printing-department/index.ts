ba9afd88984fd9e1dbf2c487dff8a16133d72073
alxmtzr <alxmtzr.dev@gmail.com>
2025-12-07 15:35:04 +0100

    solution day 4  part 1

--- src/day4-printing-department/index.ts
+++ src/day4-printing-department/index.ts
@@ -13,4 +13,62 @@ function printPaperGrid(grid: string[][]) {
     }
 }
 
-printPaperGrid(getPaperGrid());
+function calculateAmountOfRollsThatCanBeAccessed(): number {
+    const paperGrid: string[][] = getPaperGrid();
+    if (!paperGrid) return 0;
+
+    let result = 0;
+
+    for (let i = 0; i < paperGrid.length; i++) {
+        const row = paperGrid[i]!;
+        for (let j = 0; j < row.length; j++) {
+            const cell = row[j]!;
+            if (hasCellRoll(cell)) {
+                const indexOfCell = { row: i, col: j };
+                if (rollCanBeAccessed(paperGrid, indexOfCell)) {
+                    result++;
+                }
+            }
+        }
+    }
+
+
+    return result;
+}
+
+function rollCanBeAccessed(paperGrid: string[][], indexOfCell: { row: number; col: number }): boolean {
+    let count = 0;
+
+    for (let dRow = -1; dRow <= 1; dRow++) {
+        for (let dCol = -1; dCol <= 1; dCol++) {
+            // skip cell itself
+            if (dRow === 0 && dCol === 0) continue;
+
+            const newRow = indexOfCell.row + dRow;
+            const newCol = indexOfCell.col + dCol;
+
+            if (newRow >= 0 && newRow < paperGrid.length &&
+                newCol >= 0 && newCol < paperGrid[newRow]!.length) {
+                
+                if (hasCellRoll(paperGrid[newRow]![newCol]!)) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count < 4;
+}
+
+
+function hasCellRoll(cell: string): boolean {
+    return cell === "@";
+}
+
+
+function main() {
+    const resultPart1 = calculateAmountOfRollsThatCanBeAccessed();
+    console.log(`Part 1: Amount of roll that can be accessed: ${resultPart1}`);
+}
+
+main();
